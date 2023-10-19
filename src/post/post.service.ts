326b413b0258288c9e4fdecf6244393b5ba1474e
Tom Hong <tom.y.y.hong@gmail.com>
2023-10-19 19:07:45 -0400

    fixed the find Like user list in Post issues

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -64,14 +64,35 @@ export class PostService {
     }
   }
 
+  // async addLikedUser(userId: string, postId: string) {
+  //   try{
+  //     const posts = await this._postModel.find({ postId: postId }).select("likedUserList").exec();
+  //     const likedUserList = posts.post.likedUserList;
+  //     console.log("Liked User List: ", posts);
+  //   return posts;
+  //   } catch (error) {
+  //   console.log(error);
+  //   }
+  // }
+
   async addLikedUser(userId: string, postId: string) {
-    try{
-      const posts = await this._postModel.find({ postId: postId }).select("likedUserList").exec();
-      // const likedUserList = posts.likedUserList;
-      console.log("Liked User List: ", posts);
-    return posts;
+    try {
+      const post = await this._postModel
+        .findOne({ postId: postId })
+        .select("likedUserList")
+        .exec();
+  
+      if (post) {
+        const likedUserList = post.likedUserList;
+        console.log("Liked User List: ", likedUserList);
+        return likedUserList;
+      } else {
+        console.log("Post not found.");
+        return null; // or handle the case where the post is not found
+      }
     } catch (error) {
-    console.log(error);
+      console.error(error);
+      throw error;
     }
   }
 
